41ef017cde22639b93ba377069c088a2dfb3bd40
vitor.rosmann <vitor.rosmann@avmb.com.br>
2025-04-07 11:28:26 -0300

    feat: atividade 5

---
+++ src/main/java/dev/fraporti/atividade_5/model/Leitura.java
@@ -0,0 +1,24 @@
+package dev.fraporti.atividade_5.model;
+
+import java.util.Scanner;
+
+/**
+ * @author vitor.rosmann on 07/04/2025
+ */
+public final class Leitura {
+    /**
+     * Utility class, not to be instanced
+     */
+    private Leitura() {}
+
+    /**
+     * Receives a String text that will be the label thant captures the nextLine with scanner and return it as String
+     * @param label String label of the input
+     * @return String nextLine from Scanner
+     */
+    public static String entDados(String label){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(label);
+        return scanner.nextLine();
+    }
+}
